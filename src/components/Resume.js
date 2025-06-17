c905d0dbdb1252d202f1098cd41a0e9ccb0d3f79
sbugrayy <bugrabugra511@gmail.com>
2025-06-17 23:15:19 +0300

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -32,6 +32,10 @@ const ResumeSection = styled.div`
   padding: 2rem;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
+  [data-theme='dark'] & {
+    background-color: var(--background-color);
+  }
 `;
 
 const SectionTitle = styled.h3`
